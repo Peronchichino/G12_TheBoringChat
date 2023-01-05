753d893da1a97e03e459662e86ae5fa35f33e64c
buchmayerlukas <lukasbuchmayer@gmail.com>
2023-01-05 03:02:43 +0100

    Fixed stylesheet path, worked on GUI functionality

--- src/main/java/com/example/g12_theboringchat/ClientController.java
+++ src/main/java/com/example/g12_theboringchat/ClientController.java
@@ -2,33 +2,30 @@ package com.example.g12_theboringchat;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class ClientController{
     @FXML
     private Button btn_sendMessage;
     @FXML
-    private ScrollPane ScrollPane_chats;
-    @FXML
     private TextField txt_message;
     @FXML
     TextArea txt_messageArea;
 
-    private String nicknameBuf; //placeholder for now, client name
-    private String msg; //placeholder msg string
+    private String msg = "fuck";
 
-    @FXML
-    void btn_sendMessage(ActionEvent event){
-        try{
-            Socket client = new Socket("localhost", 4711);
-            txt_messageArea.appendText(nicknameBuf+": "+msg+"\n");
-        } catch(IOException e){
-            e.printStackTrace();
-        }
+    public void btnSendMsg(ActionEvent event){
+        String message = txt_message.getText();
+        txt_message.setText("");
+        txt_messageArea.appendText(message+"\n");
     }
-
-
 }
