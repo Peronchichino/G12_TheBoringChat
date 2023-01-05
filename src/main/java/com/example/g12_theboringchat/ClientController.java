3d3ed5b2e9bd1f9923c129e2c0b569d7c8ba4262
buchmayerlukas <lukasbuchmayer@gmail.com>
2023-01-05 02:05:05 +0100

    Started work on client

--- src/main/java/com/example/g12_theboringchat/ClientController.java
+++ src/main/java/com/example/g12_theboringchat/ClientController.java
@@ -1,7 +1,11 @@
 package com.example.g12_theboringchat;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+
+import java.io.IOException;
+import java.net.Socket;
 
 public class ClientController{
     @FXML
@@ -12,6 +16,19 @@ public class ClientController{
     private TextField txt_message;
     @FXML
     TextArea txt_messageArea;
+
+    private String nicknameBuf; //placeholder for now, client name
+    private String msg; //placeholder msg string
+
+    @FXML
+    void btn_sendMessage(ActionEvent event){
+        try{
+            Socket client = new Socket("localhost", 4711);
+            txt_messageArea.appendText(nicknameBuf+": "+msg+"\n");
+        } catch(IOException e){
+            e.printStackTrace();
+        }
+    }
 
 
 }
